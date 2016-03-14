fb51d70505e65047e90072b5a3270eb904a1d0ef
Dayanidhi <dayanidhicse@gmail.com>
2016-03-14 15:45:36 +0530

    Load browser
    In this we have implemented the scroll bar and brought entire browser
    into the application.

--- DataMatric/app/src/main/java/com/example/dayanidhi/datamatric/MainActivity.java
+++ DataMatric/app/src/main/java/com/example/dayanidhi/datamatric/MainActivity.java
@@ -1,16 +1,60 @@
 package com.example.dayanidhi.datamatric;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "Main";
+    private ProgressDialog progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         WebView browser = (WebView) findViewById(R.id.webview);
+
+        WebSettings settings = browser.getSettings();
+        settings.setJavaScriptEnabled(true);
+        browser.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+
+        final AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+
+        progressBar = ProgressDialog.show(MainActivity.this, "WebView Example", "Loading...");
+
+        browser.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                Log.i(TAG, "Processing webview url click...");
+                view.loadUrl(url);
+                return true;
+            }
+
+            public void onPageFinished(WebView view, String url) {
+                Log.i(TAG, "Finished loading URL: " + url);
+                if (progressBar.isShowing()) {
+                    progressBar.dismiss();
+                }
+            }
+
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Log.e(TAG, "Error: " + description);
+                Toast.makeText(MainActivity.this, "Oh no! " + description, Toast.LENGTH_SHORT).show();
+                alertDialog.setTitle("Error");
+                alertDialog.setMessage(description);
+                alertDialog.setButton("Ok",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        return;
+                    }
+                });
+                alertDialog.show();
+            }
+        });
         browser.loadUrl("https://www.google.co.in/");
     }
 }
