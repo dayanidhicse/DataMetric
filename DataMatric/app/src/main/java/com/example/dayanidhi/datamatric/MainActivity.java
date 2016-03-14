4fab3402d5cb3d4682bff7411a9c6a591d7e1895
Dayanidhi <dayanidhicse@gmail.com>
2016-03-14 15:23:42 +0530

    Initial setup
    This contains web view integration.

---
+++ DataMatric/app/src/main/java/com/example/dayanidhi/datamatric/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.dayanidhi.datamatric;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView browser = (WebView) findViewById(R.id.webview);
+        browser.loadUrl("https://www.google.co.in/");
+    }
+}
