250c99e273f7db49c31f47e2223ce5a87ef0e8f1
Dayanidhi <dayanidhicse@gmail.com>
2016-03-14 23:31:49 +0530

    url with data used
    Url with data used is found in this commit.

---
+++ DataMatric/app/src/main/java/com/example/dayanidhi/datamatric/Data_Table.java
@@ -0,0 +1,13 @@
+package com.example.dayanidhi.datamatric;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Data_Table extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data__table);
+    }
+}
